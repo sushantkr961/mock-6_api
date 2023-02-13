c3cdc59d24c0122f011f0976f233b5905a58faa5
sushantkr961 <sushantonly961@gmail.com>
2023-02-14 03:36:09 +0530

    done

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
 server.use(middlewares);
 server.use(router);
+server.use(cors());
 
 server.listen(8080, () => {
   console.log("JSON Server is running");
